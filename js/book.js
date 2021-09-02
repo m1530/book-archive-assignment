6b15823e64f08892a6831058625daedb980f56cd
Md. Moshiur Rahman <rahmanmoshiur253@gmail.com>
2021-09-02 21:54:44 +0600

    committ at 10p

--- js/book.js
+++ js/book.js
@@ -5,11 +5,10 @@ const searchBook = () => {
     //validation for empty value
     if (searchText === '') {
         document.getElementById('error-message').innerText = `
-            Search field cann't empty. please write something
+            Search field cann't empty. please write valid book name.
         `;
         displaySpinner('none');
         toggleSearchResult('none');
-        //document.getElementById('total-found').innerText = '';
         getInnerText('total-found');
     } else {
         // display spinner
@@ -17,12 +16,9 @@ const searchBook = () => {
         toggleSearchResult('none');
         loadBooks(searchText);
         document.getElementById('search-field').value = '';
-        /*   document.getElementById('error-message').innerText = '';
-          document.getElementById('total-found').innerText = ''; */
         getInnerText('error-message');
         getInnerText('total-found');
     }
-
 }
 // function for common empty innertext 
 const getInnerText = id => {
@@ -39,42 +35,42 @@ const toggleSearchResult = displayStyle => {
 
 //lood books from url
 const loadBooks = searchText => {
-    const url = `http://openlibrary.org/search.json?q=${searchText}`;
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
         .then(res => res.json())
         .then(data => {
             displayBooks(data.docs);
-            document.getElementById('total-found').innerText = `${data.numFound}`;
+            document.getElementById('total-found').innerText = `Out of ${data.numFound}`;
         })
 }
 //display book 
 const displayBooks = books => {
+    //console.log(books);
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
     //validation for not finding book
     if (books.length === 0) {
         document.getElementById('error-message').innerText = `
-        Not found. please search valid book name!!
+        No result found. please search valid book name!!
     `;
     }
-    books?.forEach(book => {
-
-        let author;
-        //check author and display one author name
-        if (book.author_name) {
-            author = book.author_name[0];
-        }
+    //make slice and show 35 books
+    const lessBook = books.slice(0, 35);
+    document.getElementById('show').innerText = `Total Shows ${lessBook.length}`;
+    lessBook?.forEach(book => {
+        //console.log(book)
+        book.cover_i ? cover = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg` : cover = `image/1.jpg`;
         //console.log(book);
         const divCol = document.createElement('div');
         divCol.classList.add('col');
         divCol.innerHTML = `
             <div class="card h-100">
-                <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
+                <img src="${cover}" class="card-img-top" alt="Image not found">
                 <div class="card-body">
-                    <h5 class="card-title">${book.title}</h5>
-                    <h4>by: ${author}</h4>
-                    <h6>Publisher: ${book.publisher}</h6>
-                    <p>First published in ${book.first_publish_year}</p>
+                    <h5 class="card-title fw-bold">${book.title ? book.title : 'N/A'}</h5>
+                    <h6>by: ${book.author_name ? book.author_name[0] : 'N/A'}</h6>
+                    <h6>Publisher: ${book.publisher ? book.publisher[0] : 'N/A'}</h6>
+                    <p>First published in: ${book.first_publish_year ? book.first_publish_year : ''}</p>
                 </div>
             </div>
         `;
